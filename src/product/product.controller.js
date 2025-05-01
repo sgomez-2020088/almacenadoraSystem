4dd77ccbcadfd296a3cb73823a3bc6acd8147c2c
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-05-01 17:39:35 -0600

    se agregan funcionalidades crud

---
+++ src/product/product.controller.js
@@ -0,0 +1,71 @@
+import Product from './product.model.js'
+
+export const addProduct = async (req, res) => {
+    try {
+        const data = req.body
+        const userId = req.user.uid
+        const product = new Product({
+            ...data,
+            incharge: req.user.uid
+        })
+        await product.save()
+
+        const allInfo = await Product.findById(product._id)
+        .populate('category', 'name -_id')
+        .populate('supplier', 'name -_id')
+        .populate('incharge', 'name surname -_id')
+    
+        return res.send({message: 'Product added successfully',success: true, allInfo
+        })
+    } catch (err) {
+        console.error(err)
+        return res.status(500).send({ message: 'General error adding product' })
+    }
+}
+
+export const getAll = async (req, res) => {
+    try {
+        const products = await Product.find({
+            status: { $ne: false }
+        })
+        .populate('category', 'name -_id')
+        .populate('supplier', 'name -_id')
+        .populate('incharge', 'name surname -_id')
+
+        if (products.length === 0) return res.status(400).send({ message: 'Products not found', success: false })
+        return res.send({ message: 'Products found', success: true, products })
+    } catch (err) {
+        console.error(err)
+        res.status(500).send({ message: 'General error', success: false })
+    }
+}
+
+export const updateProduct = async (req, res) => {
+    try {
+        const {id} = req.body
+        const data = req.body
+        const updated = await Product.findByIdAndUpdate (id, data,  { new: true })
+        .populate('category', 'name -_id')
+        .populate('supplier', 'name -_id')
+        .populate('incharge', 'name surname -_id')
+        if (!updated) return res.status(404).send({ message: 'Product not found', success: false })
+        return res.send({ message: 'Product updated successfully', success: true, product: updated })
+    }
+    catch (err) {
+        console.error(err)
+        res.status(500).send({ message: 'General error updating product', success: false })
+    }
+}
+
+export const deleteProduct = async (req, res) => {
+    try {
+        const { id } = req.body
+        const product = await Product.findByIdAndUpdate(id, { status: false }, { new: true })
+        if (!product) return res.status(404).send({ message: 'Product not found', success: false })     
+        return res.send({ message: 'Product deleted successfully', success: true, product })
+    }
+    catch (err) {
+        console.error(err)
+        return res.status(500).send({ message: 'General error deleting product', success: false })
+    }   
+}
