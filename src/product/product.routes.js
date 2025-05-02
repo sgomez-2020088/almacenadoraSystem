77de893b2f88364af86a90900faf26b62befb8dc
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-05-01 19:11:04 -0600

    se actualizan rutas

--- src/product/product.routes.js
+++ src/product/product.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { addProduct, getAll, updateProduct, deleteProduct, getByCategory, getByName,getByDate } from './product.controller.js'
+import { addProduct, getAll, updateProduct, deleteProduct, getByCategory, getByName,getByDate,getStockProducto } from './product.controller.js'
 import {validateJwt } from '../../middlewares/validate.jwt.js'
 
 const api = Router()
@@ -14,6 +14,7 @@ api.delete('/delete',[validateJwt],deleteProduct)
 api.get('/category',[validateJwt],getByCategory)
 api.get('/name',[validateJwt],getByName)
 api.get('/date',[validateJwt],getByDate)
+api.get('/stock',[validateJwt],getStockProducto)
 
 export default api
 
