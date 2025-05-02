e4a935898e64db8e559438a0783ac4752ac0c84d
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-05-01 23:00:04 -0600

    se actualizan rutas

--- src/report/report.routes.js
+++ src/report/report.routes.js
@@ -1,14 +1,14 @@
-import { Router } from "express";
-import { validateJwt } from "../../middlewares/validate.jwt.js";
-import { addRegistry, getAllReports, getEntryReports, getExitReports, getOneReport } from "./report.controller.js";
-
+import { Router } from "express"
+import { validateJwt,isAdmin } from "../../middlewares/validate.jwt.js"
+import { addRegistry, getAllReports, getEntryReports, getExitReports, getOneReport } from "./report.controller.js"
+import { addReportValidator } from "../../helpers/validators.js"
 const api = Router()
 
-api.post('/',[validateJwt], addRegistry)
+api.post('/',[validateJwt,isAdmin,addReportValidator], addRegistry)
 
-api.get('/all',[validateJwt], getAllReports)
-api.get('/entry',[validateJwt], getEntryReports)
-api.get('/exit',[validateJwt], getExitReports)
-api.get('/:id',[validateJwt], getOneReport)
+api.get('/all',[validateJwt,isAdmin], getAllReports)
+api.get('/entry',[validateJwt,isAdmin], getEntryReports)
+api.get('/exit',[validateJwt,isAdmin], getExitReports)
+api.get('/:id',[validateJwt,isAdmin], getOneReport)
 
 export default api
