f29cd38c9d9cae5119a375c9c45ac2ec13e27270
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-05-01 22:59:51 -0600

    se actualizan rutas

--- src/supplier/supplier.routes.js
+++ src/supplier/supplier.routes.js
@@ -1,16 +1,16 @@
 import { Router } from 'express'
 import { addSupplier,getAll, deleteSupplier, updateSupplier} from './supplier.controller.js'
-import {validateJwt } from '../../middlewares/validate.jwt.js'
+import {validateJwt,isAdmin } from '../../middlewares/validate.jwt.js'
 import { addSupplierValidator, UpdateSupplierValidator, deleteSupplierValidator } from '../../helpers/validators.js'
 
 const api = Router()
 
-api.post('/add',[validateJwt, addSupplierValidator ],addSupplier)
+api.post('/add',[validateJwt,isAdmin, addSupplierValidator ],addSupplier)
 
 api.get('/all',[validateJwt],getAll)
 
-api.put('/update',[validateJwt, UpdateSupplierValidator],updateSupplier)
-api.delete('/delete',[validateJwt,deleteSupplierValidator],deleteSupplier)
+api.put('/update',[validateJwt,isAdmin,UpdateSupplierValidator],updateSupplier)
+api.delete('/delete',[validateJwt,isAdmin,deleteSupplierValidator],deleteSupplier)
 
 export default api
 
