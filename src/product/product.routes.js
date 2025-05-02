b7fc2ea1bb531d73c40b71aa216b1b569c6775b7
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-05-01 22:59:40 -0600

    se actualizan rutas

--- src/product/product.routes.js
+++ src/product/product.routes.js
@@ -1,20 +1,43 @@
 import { Router } from 'express'
-import { addProduct, getAll, updateProduct, deleteProduct, getByCategory, getByName,getByDate,getStockProducto } from './product.controller.js'
-import {validateJwt } from '../../middlewares/validate.jwt.js'
+import { 
+    addProduct, 
+    getAll, 
+    updateProduct, 
+    deleteProduct,
+     getByCategory, 
+     getByName,
+     getByDate,
+     getStockProducto,
+     getAllStock,
+     getInvetory 
+    } from './product.controller.js'
+    
+import {validateJwt,isAdmin } from '../../middlewares/validate.jwt.js'
+
+import { addProductValidator, 
+    updateProductValidator, 
+    deleteProductValidator,
+    productCategoryValidator,
+    productNameValidator,
+    productDateValidator,
+    productStockValidator 
+} from '../../helpers/validators.js'
 
 const api = Router()
 
-api.post('/add',[validateJwt ],addProduct)
+api.post('/add',[validateJwt,addProductValidator ],addProduct)
 
 api.get('/all',[validateJwt],getAll)
 
-api.put('/update',[validateJwt],updateProduct)
-api.delete('/delete',[validateJwt],deleteProduct)
+api.put('/update',[validateJwt,isAdmin,updateProductValidator],updateProduct)
+api.delete('/delete',[validateJwt,isAdmin,deleteProductValidator],deleteProduct)
 
-api.get('/category',[validateJwt],getByCategory)
-api.get('/name',[validateJwt],getByName)
-api.get('/date',[validateJwt],getByDate)
-api.get('/stock',[validateJwt],getStockProducto)
+api.get('/category',[validateJwt,productCategoryValidator],getByCategory)
+api.get('/name',[validateJwt,productNameValidator],getByName)
+api.get('/date',[validateJwt,productDateValidator],getByDate)
+api.get('/stock',[validateJwt,productStockValidator],getStockProducto)
+api.get('/allStock',[validateJwt],getAllStock)
+api.get('/inventory',[validateJwt],getInvetory)
 
 export default api
 
