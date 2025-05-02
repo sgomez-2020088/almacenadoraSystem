f0d90ce3c5482d0579a44b5a0f4849a88faddcbf
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-05-01 22:59:18 -0600

    se actualizan rutas

--- src/customer/customer.routes.js
+++ src/customer/customer.routes.js
@@ -1,16 +1,16 @@
 import { Router } from 'express'
-import {validateJwt } from '../../middlewares/validate.jwt.js'
+import {validateJwt,isAdmin} from '../../middlewares/validate.jwt.js'
 import { addCustomer, getAll, updateCustomer, deleteCustomer} from './customer.controller.js'
 import { addCustomerValidator, updateCustomerValidator, deleteCustomerValidator } from '../../helpers/validators.js'
 
 const api = Router()
 
-api.post('/add',[validateJwt, addCustomerValidator ], addCustomer)
+api.post('/add',[validateJwt,isAdmin,addCustomerValidator ], addCustomer)
 
 api.get('/all',[validateJwt],getAll)
 
-api.put('/update',[validateJwt, updateCustomerValidator],updateCustomer)
-api.delete('/delete',[validateJwt, deleteCustomerValidator],deleteCustomer)
+api.put('/update',[validateJwt,isAdmin,updateCustomerValidator],updateCustomer)
+api.delete('/delete',[validateJwt,isAdmin,deleteCustomerValidator],deleteCustomer)
 
 export default api
 
