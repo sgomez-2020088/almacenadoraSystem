08a7ba52dd8dbf8e5fdfef6ef6baba3fef6042da
jgutierrez <jgutierrez-2020370@kinal.edu.gt>
2025-05-01 22:25:20 -0600

    Buscar por rango de fechas, añadido

--- src/report/report.routes.js
+++ src/report/report.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { validateJwt } from "../../middlewares/validate.jwt.js";
-import { addRegistry, getAllReports, getEntryReports, getExitReports, getOneReport } from "./report.controller.js";
+import { addRegistry, getAllReports, getByDate, getEntryReports, getExitReports, getOneReport } from "./report.controller.js";
 
 const api = Router()
 
@@ -9,6 +9,7 @@ api.post('/',[validateJwt], addRegistry)
 api.get('/all',[validateJwt], getAllReports)
 api.get('/entry',[validateJwt], getEntryReports)
 api.get('/exit',[validateJwt], getExitReports)
+api.get('/date',[validateJwt], getByDate)
 api.get('/:id',[validateJwt], getOneReport)
 
 export default api
