af1ef6d5363c8270230cd21a5ad8e2b2b6b1f690
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-05-01 22:58:39 -0600

    se agrega validacion para administradores

--- middlewares/validate.jwt.js
+++ middlewares/validate.jwt.js
@@ -1,7 +1,6 @@
 'use strict '
 import jwt from 'jsonwebtoken'
 
-
 export const validateJwt = async(req, res, next) =>{
     try {
         let secretKey = process.env.SECRET_KEY
@@ -20,13 +19,15 @@ export const validateJwt = async(req, res, next) =>{
 
 export const isAdmin = async(req, res, next)=>{
     try{
-        const { user } = req
+        const {user} = req
+        console.log(user)
         if(!user || user.role !== 'ADMIN') return res.status(403).send(
             {
                 success: false,
-                message: `You dont have access | email ${user.email}`
+                message: `You dont have access | username ${user.username}`
             }
         )
+        
         next()
     }catch(err){
         console.error(err)
