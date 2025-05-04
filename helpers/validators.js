09ff25a1741fa2c0dc52ac234c5ea2ef84710763
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-05-03 18:46:04 -0600

    Se agrega la nueva validación

--- helpers/validators.js
+++ helpers/validators.js
@@ -1,13 +1,13 @@
 import { body, ExpressValidator } from 'express-validator' 
 import { validateErrors} from './validate.errors.js'
-import { exitEmailUser, exitEmailCustomer, exitEmailSupplier } from './db.validators.js'
+import { exitEmailUser, exitEmailCustomer, exitEmailSupplier, exitsUsername } from './db.validators.js'
 
 export const registerValidator = [
     body('name', 'Name cannot be empty').notEmpty(),
     body('surname', 'Name cannot be empty').notEmpty(),
     body('email', 'Email cannot be empty').notEmpty().isEmail().custom(exitEmailUser),
     body('password', 'Password cannot be empty').notEmpty().isStrongPassword().withMessage('Password must be strong').isLength({min:8}),
-    body('username', 'Username cannot be empty').notEmpty(),
+    body('username', 'Username cannot be empty').notEmpty().custom(exitsUsername),
     body('phone', 'Phone cannot be empty').notEmpty().isMobilePhone(),
     validateErrors       
 ]
